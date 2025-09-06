69424f6fcf4004a48c09cd6739b2eaa019875e8d
Dariel <darielgutierrez332@gmail.com>
2025-09-05 22:14:13 -0700

    changed favicon - added hover effect to project cards

--- wcofall25/src/components/ProjectShowcase/ProjectShowcase.jsx
+++ wcofall25/src/components/ProjectShowcase/ProjectShowcase.jsx
@@ -14,6 +14,7 @@ export default function ProjectShowcase({
       initial={{ opacity: 0, y: 30 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.4 }}
+      whileHover={{ scale: 1.1 }}
     >
       <Card
         sx={{
